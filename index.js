079f9f3453d30785f65ee221c3f658eee2d640e6
Griffin Yourick <gryphon92@gmail.com>
2019-11-20 12:09:02 -0500

    consolidate index.js and eslintrc

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
 'use strict';
 
-module.exports = require('./.eslintrc.json');
+module.exports = {
+  env: {
+    node: true,
+    mocha: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: { ecmaVersion: 2018 },
+  plugins: ['mocha', 'node', 'prettier'],
+  rules: {
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-spacing': 'error',
+    'object-shorthand': ['error', 'always'],
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'prefer-destructuring': [
+      'error',
+      {
+        array: false,
+        object: true
+      }
+    ],
+    complexity: ['error', 10],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'node/shebang': 'off',
+    'linebreak-style': ['error', 'unix'],
+    'no-unused-vars': ['error', { args: 'none' }],
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        ignores: []
+      }
+    ],
+    'node/no-unsupported-features/es-builtins': [
+      'error',
+      {
+        ignores: []
+      }
+    ],
+    'node/no-deprecated-api': [
+      'warn',
+      {
+        ignoreModuleItems: ['domain', 'process.binding']
+      }
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        singleQuote: true,
+        tabWidth: 2,
+        useTabs: false
+      }
+    ],
+    'mocha/no-exclusive-tests': 'error'
+  }
+};
